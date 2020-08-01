0f19df1910540e9147d039746afb07c21912450f
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2020-08-01 21:45:36 +0900

    fix lint

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	a := []int{0, 1, 2}
-	for i, _ := range a {
+	for i := range a {
 		fmt.Println(strconv.Itoa(i))
 		for j := 0; j < len(a); j++ {
 			fmt.Println(strconv.Itoa(j) + ": " + strconv.Itoa((i+j)%3))
