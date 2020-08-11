d3410c5242ad243cedf385ef990d87320ac6e6e8
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2020-08-11 22:34:18 +0900

    add tests

---
+++ internal/code/code-structure-base_test.go
@@ -0,0 +1,68 @@
+package code
+
+import (
+	"github.com/duosonic62/codanalyzer-domains/internal/tone"
+	"testing"
+)
+
+var majorSeventh, _ = NewCodeStructureBase("M7", majorSeventhIntervals())
+
+func TestNewCodeStructureBase_Positive(t *testing.T) {
+	_, err := NewCodeStructureBase("M7", majorSeventhIntervals())
+	if err != nil {
+		t.Error("Expected error is nil but, actual: ", err)
+	}
+}
+
+func TestNewCodeStructureBase_Negative(t *testing.T) {
+	intervals := []tone.Interval{
+		*tone.NewInterval(0),
+		*tone.NewInterval(1),
+	}
+
+	//コード構成音が2音以下
+	_, err := NewCodeStructureBase("M7", &intervals)
+	if err == nil {
+		t.Error("Expected error not nil but, actual: ", err)
+	}
+}
+
+func TestStructureBase_Name_Positive(t *testing.T) {
+	if majorSeventh.Name() != "M7" {
+		t.Error("Expected M7 but, actual: ", majorSeventh.Name())
+	}
+}
+
+
+func TestStructureBase_GetIntervals_Positive(t *testing.T) {
+	expected := *majorSeventhIntervals()
+
+	for i, actual := range *majorSeventh.intervals {
+		if actual != expected[i] {
+			t.Error("Expected: ", expected[i], ", but actual: ", actual)
+		}
+	}
+}
+
+func TestStructureBase_GetCode_Positive(t *testing.T) {
+	actual := majorSeventh.GetCode(&tone.ScaleTones.C)
+	expected, err := NewCode("M7", majorSeventhIntervals(), &tone.ScaleTones.C)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if *actual.Name != *expected.Name {
+		t.Error("Expected: " + expected.Name.Value + ", but actual: " + actual.Name.Value)
+	}
+
+	if *actual.Root != *expected.Root {
+		t.Error("Expected: " + expected.Root.String() + ", but actual: " + actual.Root.String())
+	}
+
+	for i, actualTone := range *actual.Tones {
+		expectedTones := *expected.Tones
+		if actualTone != expectedTones[i] {
+			t.Error("Expected: " + expectedTones[i].String() + ", but actual: " + actualTone.String())
+		}
+	}
+}
