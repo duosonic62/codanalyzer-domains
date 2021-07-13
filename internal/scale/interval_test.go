acb6bda82ad1b3166aec37f6ea907bc622450f14
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-07-13 21:16:18 +0900

    add is equals tests

--- internal/scale/interval_test.go
+++ internal/scale/interval_test.go
@@ -17,3 +17,17 @@ func TestInterval_IsEquivalent(t *testing.T) {
 		t.Error("Expected: R and 2 is not Equivalent")
 	}
 }
+
+func TestInterval_IsEquals(t *testing.T) {
+	a := Intervals.Natural9
+	b := Intervals.Major2
+
+	if a.IsEquals(&b) {
+		t.Error("Expected: 2 and 9 is not Equals")
+	}
+
+	c := Intervals.Natural9
+	if !a.IsEquals(&c) {
+		t.Error("Expected: 9 and 9 is Equals")
+	}
+}
